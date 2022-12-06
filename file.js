eee4429d06bfbc61fdb3adde8fca88737dcf0b36
BANAFAI Romeo <romeobanafai5@gmail.com>
2022-12-06 00:38:09 +0000

    premier commit

---
+++ file.js
@@ -0,0 +1,8 @@
+
+const modess = document.querySelector('.modess');
+const nav = document.querySelector('nav');
+
+modess.addEventListener('click',()=>{
+modess.classList.toggle('active')
+nav.classList.toggle('active')
+})
